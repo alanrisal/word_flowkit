896b9204c4292d1370e467c8e132c354a9d5bf0b
genecodAlan <risalgenetics@gmail.com>
2026-03-16 18:02:29 -0400

    publix fix

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/taskpane.css";
 
-Office.onReady(() => {
+function mountApp() {
   const container = document.getElementById("root");
-  if (!container) {
-    throw new Error("Could not find #root element");
-  }
-  const root = ReactDOM.createRoot(container);
-  root.render(<App />);
-});
+  if (!container) return;
+  ReactDOM.createRoot(container).render(<App />);
+}
+
+if (typeof Office !== "undefined") {
+  Office.onReady(mountApp);
+} else {
+  mountApp();
+}
