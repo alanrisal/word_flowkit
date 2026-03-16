85cdb4f1ca58d310025380557939a9c4288cc37e
genecodAlan <risalgenetics@gmail.com>
2026-03-16 17:59:21 -0400

    fix publix path

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,57 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const devCerts = require("office-addin-dev-certs");
 
-module.exports = {
-  entry: "./src/taskpane/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "taskpane.bundle.js",
-    publicPath: process.env.NODE_ENV === "production" ? "/Word_flowkit/" : "/",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/taskpane/index.html",
-      filename: "index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  const config = {
+    entry: "./src/taskpane/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "docs"),
+      filename: "taskpane.bundle.js",
+      publicPath: isProd ? "/Word_flowkit/" : "/",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
         {
-          from: "assets",
-          to: "assets",
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
       ],
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    server: {
-      type: "https",
-      options: devCerts.getHttpsServerOptions(),
     },
-    hot: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/taskpane/index.html",
+        filename: "index.html",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "assets", to: "assets" }],
+      }),
+    ],
+  };
+
+  if (!isProd) {
+    const devCerts = require("office-addin-dev-certs");
+    config.devServer = {
+      port: 3000,
+      server: {
+        type: "https",
+        options: devCerts.getHttpsServerOptions(),
+      },
+      hot: true,
+      headers: { "Access-Control-Allow-Origin": "*" },
+    };
+  }
+
+  return config;
 };
